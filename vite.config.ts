890d8b80e4648d448daa18f285fe93fa23817933
kim001hs <kim001hs@gmail.com>
2026-01-08 21:16:06 +0900

    fix: 클라우드 프론트 병합을 위해 basepath추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/storead/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
